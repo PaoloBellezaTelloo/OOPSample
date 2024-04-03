fd508dc9915109df30f9bf7056ac18f74d42bcd8
Paolo <U20191A137@upc.edu.pe>
2024-04-03 11:42:03 -0500

    feat(sales-order): added initial definition for sales order aggregate.

---
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -0,0 +1,7 @@
+﻿namespace OOPSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrder(int customerId)
+{
+    public Guid Id { get; } = Guid.NewGuid(); // Guid sirve para generar claves aleatorias
+    public int CustomerId { get; } = customerId;
+}
